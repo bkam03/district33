a1edce23f3fade1bd379962d78b266097f8b45b3
BK <bkam03@yahoo.com>
2018-02-13 19:44:29 -1000

    got photo path correct, now displays

--- public/js/galleryData.js
+++ public/js/galleryData.js
@@ -3,24 +3,29 @@ const fs = require("fs");
 const galleryArray = [
   {
     eventName: "HEW Meeting",
+    folder: "HEWMeeting",
     description:
       "Sam Kong and Architect Russell Wozniak presenting their Under and Upwards homeless housing project at Pearl City's Health, Education, and Welfare (HEW) Committee Hearing at Lehua Elementary on January 26, 2018."
   },
   {
     eventName: "Milken Educator Award",
+    folder: "MilkenEducatorAward",
     description: "########################################################"
   },
   {
     eventName: "Pearl Harbor Survivor Funeral Service",
+    folder: "PearlHarborSurvivorFuneralService",
     description:
       "Sam Kong at the funeral service for Pearl Harbor survivor Estelle Birdsell at Airzona Memorial on December 5, 2017."
   },
   {
     eventName: "Volcano Elementary Kids Visit",
+    folder: "VolcanoElementaryKidsVisit",
     description: "Sam Kong and students from Volcano School."
   },
   {
     eventName: "Veteran's Caucus",
+    folder: "Veteran'sCaucus",
     description:
       "Pictures from the Veterans' Caucus hosted by Representative Kong and Representative Gene Ward that was held on January 18, 2018.   Legislators and other guests came to learn what US Vets is doing for our veterans."
   }
@@ -28,13 +33,14 @@ const galleryArray = [
 
 for (var i = 0; i < galleryArray.length; i++) {
   let currentGallery = galleryArray[i];
-  let currentGalleryPath = `public/resources/${currentGallery.eventName}/`;
+
+  let currentGalleryPath = `./public/resources/${currentGallery.folder}/`;
   let listOfPhotosInDirectory = fs.readdirSync(currentGalleryPath);
   currentGallery.photos = listOfPhotosInDirectory;
 
   for (var p = 0; p < currentGallery.photos.length; p++) {
     currentGallery.photos[p] = {
-      photo: currentGalleryPath + currentGallery.photos[p]
+      photo: `resources/${currentGallery.folder}/${currentGallery.photos[p]}`
     };
   }
 }
