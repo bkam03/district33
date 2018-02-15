ed557cb2e747fee3c38a4b1e0e1f96d1c0df3f30
BK <bkam03@yahoo.com>
2018-02-14 19:01:47 -1000

    changed let to const on line 6

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
 
-let galleryData = require("./public/js/galleryData.js");
+const galleryData = require("./public/js/galleryData.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
