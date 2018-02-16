98f16424d35ba611b67c396a43ddc20d843fc02e
BK <bkam03@yahoo.com>
2018-02-15 19:33:35 -1000

    added raw data to file, populated and tested

--- public/js/issuesData.js
+++ public/js/issuesData.js
@@ -2,9 +2,28 @@
 
 const issuesArray = [
   {
-    issue: "Issue",
-    subheading: "important",
-    description: "Important things will be said here."
+    issue: "Education",
+    subheading: "education",
+    description:
+      "When educators developed the SCBM School community-based management (20 years), I became Webling Elementary School's Community Representative for the Board. I have been there ever since.  Upon being elected, I no longer could be the community representative for the School Community Council (SCC), but I still attend the meetings regularly though I have no voting rights.  Being on the council allows me to be stay informed with all that goes on and also helps the school as it transitions through different administrations.</br> Having two children go through the Aiea school system also has kept me abreast with the intermediate and high school levels.  After all, any parent knows how important it is to be involved with their keiki, even when they don't want it. My last has just graduated from Aiea High School.  I have already met with the current principal on several occasions to brief me in on what the school needs.  More importantly, I know what the schools do not need! Twenty years of involvement can do that to you. "
+  },
+  {
+    issue: "Community",
+    subheading: "people",
+    description:
+      "Back to the Graffiti Busters- As a volunteer, we were so good. We had distributed so much paint, that we had graffiti under control for a long time. So we disbanded.  Well, all good things come to an end, I guess.  Graffiti came back but someone was beating me to it and covering it up!  I found out who when I got elected but I fell ill so I couldn't participate. We did get together later on though and I joined their team- The Lions Club.   Lions Club members from different clubs get together to do different projects taking care of the community.</br> Wanting to focus on Aiea, we started doing many projects in my district, well from Salt Lake to Pearl City to be honest.  Though we do go out to Kailua, Kalihi and Chinatown on occasion.  We have done so many projects in Aiea and Pearl City each Saturday morning, that I've lost count.  But I couldn't imagine what our community would look like if we didn't.   From graffiti painting, landscaping, to just picking up trash, Lions have done so much that you may have not even noticed.  Thank you to those also who have done things out there in the community.  Oh yes. I may not know who you are, but I do know the work you do. It has not gone unnoticed. "
+  },
+  {
+    issue: "Business",
+    subheading: "local",
+    description:
+      "The business world presents its own challenges.  Being a local businessman, I know one has to deal with so much! From government and taxes, to the economy, the employees and their benefits- you name it.  I can relate! Looking back on 21 years of owning Aiea Florist, I do not regret a day of it! Wish my health didn't fail me and put me in a position to sell, but I did.  What I miss most is not designing and working with flowers, but being with my friends.  Fortunately, being an elected official allows me to be in contact with them, but it is not the same."
+  },
+  {
+    issue: "Homeless",
+    subheading: "ness",
+    description:
+      " My goal is to truly address the homeless situation. After personally assisting and working with the homeless in the Aiea community for the past twenty five years, I have made it my priority to solve this problem since being elected.  Our plan started with developing a team of government officials and community and business leaders who could implement the solution of providing housing for the homeless.  We have identified properties throughout Oahu (City, State, and Federal) for possible development. Our first project is 'Under and Upwards', a six acre DOT property located under the freeway in Pearl City off of Hugh St.  The second project is the Cedar Church 4 acre property located on Waianae Valley Road.  The third project is Hoomau Ke Ola, a 1,000-acre parcel serving the chronically ill/drug abuser, also located in Waianae.  The fourth project is a 3 ½ acre federal property focused on veterans.  By doing all these projects, we will be able to house the homeless at a significant lower cost, and a great benefit to the homeless community.</br>My vision for education and community comes down to supporting the family- not supporting the government. "
   }
 ];
 
