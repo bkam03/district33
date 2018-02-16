6ccbe2d8134c5e590d3a8511b784a439eecbcc77
BK <bkam03@yahoo.com>
2018-02-15 19:27:11 -1000

    created data file to hold issues

---
+++ public/js/issuesData.js
@@ -0,0 +1,17 @@
+//const fs = require("fs");
+
+const issuesArray = [
+  {
+    issue: "Issue",
+    subheading: "important",
+    description: "Important things will be said here."
+  }
+];
+
+var issuesData = {
+  issues: issuesArray
+};
+
+//console.log(galleryData.galleries[1]);
+
+module.exports = issuesData;
