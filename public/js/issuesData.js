a6e067a082d721edcbefb03293ef5dd606443870
BK <bkam03@yahoo.com>
2018-03-11 11:21:44 -1000

    changed description format to include optional list, removed tags from text

--- public/js/issuesData.js
+++ public/js/issuesData.js
@@ -3,28 +3,56 @@
 const issuesArray = [
   {
     issue: "Education",
-    subheading: "education",
+    subheading: "",
     link: "education",
     description:
-      "Sam believes hawaii’s children deserve a high quality education provided by engaged, empowered teachers.  Sam is currently Vice Chairman if the House Education Committee. He has also been formally and informally involved in the curriculum development, operations and facility maintenance of the schools in Aiea and Halawa.   As a community member Sam has been on the board of Gus Webling Elementary  School.  As a legislator  sam has obtained the following funding and/or support for projects.As a member of the House Education Committer Sam hopes to advocate for both students and teachers through his role on this pivotal committee. He is committed to providing both high quality educational opportunities to Hawaii’s children and supporting Hawaii’s teachers.</br>CIP PROJECTS</br> Pearl Ridge Elementary School</br> Design and construction for air conditioning improvements for buildings H & J</br> Total Funding:  1,166,500.00.</br>Aiea  Intermediate School</br>Design and construction for improvements to the cafeteria stage.</br>Total Funding: $450,000</br>Webling Elementary School</br>To finance construction for new playground equipment and site improvements.</br>Total Funding: $125,000</br>Aiea High School</br>Design, construction, and equipment for girls’ athletic locker room, and other athletic facilities.</br>Total  Funding: $3,900,000"
+      "Sam believes hawaii’s children deserve a high quality education provided by engaged, empowered teachers.  Sam is currently Vice Chairman if the House Education Committee. He has also been formally and informally involved in the curriculum development, operations and facility maintenance of the schools in Aiea and Halawa.   As a community member Sam has been on the board of Gus Webling Elementary  School.  As a legislator  sam has obtained the following funding and/or support for projects.As a member of the House Education Committer Sam hopes to advocate for both students and teachers through his role on this pivotal committee. He is committed to providing both high quality educational opportunities to Hawaii’s children and supporting Hawaii’s teachers.",
+
+    listTitle: "CIP Projects",
+    listItems: [
+      {
+        itemTitle: "Pearl Ridge Elementary School",
+        itemDescription:
+          "Design and construction for air conditioning improvements for buildings H & J",
+        itemEnd: "Total Funding:  1,166,500.00"
+      },
+      {
+        itemTitle: "Aiea  Intermediate School",
+        itemDescription:
+          "Design and construction for improvements to the cafeteria stage.",
+        itemEnd: "Total Funding: $450,000"
+      },
+      {
+        itemTitle: "Webling Elementary School",
+        itemDescription:
+          "To finance construction for new playground equipment and site improvements.",
+        itemEnd: "Total Funding: $125,000"
+      },
+      {
+        itemTitle: "Aiea High School",
+        itemDescription:
+          "Design, construction, and equipment for girls’ athletic locker room, and other athletic facilities.",
+        itemEnd: "Total  Funding: $3,900,000"
+      }
+    ]
   },
   {
     issue: "Community",
-    subheading: "people",
+    subheading: "",
     link: "/community",
     description:
       "Back to the Graffiti Busters- As a volunteer, we were so good. We had distributed so much paint that we had graffiti under control for a long time. So we disbanded.  Well, all good things come to an end, I guess.  Graffiti came back but someone was beating me to it and covering it up!  I found out who when I got elected but I fell ill so I couldn't participate. We did get together later on though and I joined their team- The Lions Club.   Lions Club members from different clubs get together to do different projects taking care of the community.</br> Wanting to focus on Aiea, we started doing many projects in my district. Well from Salt Lake to Pearl City to be honest.  Though we do go out to Kailua, Kalihi and Chinatown on occasion.  We have done so many projects in Aiea and Pearl City each Saturday morning, that I've lost count.  But I couldn't imagine what our community would look like if we didn't.   From graffiti painting, landscaping, to just picking up trash, Lions have done so much that you may have not even noticed.  Thank you to those also who have done things out there in the community.  Oh yes. I may not know who you are, but I do know the work you do. It has not gone unnoticed."
   },
   {
     issue: "Business",
-    subheading: "local",
+    subheading: "",
     link: "/business",
     description:
       "The business world presents its own challenges.  Being a local businessman, I know one has to deal with so much! From government and taxes, to the economy, the employees and their benefits- you name it.  I can relate! Looking back on 21 years of owning Aiea Florist, I do not regret a day of it! Wish my health didn't fail me and put me in a position to sell, but it did.  What I miss most is not designing and working with flowers, but being with my friends.  Fortunately, being an elected official allows me to be in contact with them, but it is not the same."
   },
   {
     issue: "Homeless",
-    subheading: "ness",
+    subheading: "",
     link: "/homeless",
     description:
       "Despite the state's efforts in providing housing, homelessness continues to rise in Hawaii.  According to the Department of Urban Development, Hawaii has the highest rate of homelessness per capita in the United States. Homelessness is a complex issue with multiple causes. Sam strongly believes that housing is a  key priority.   He has personally been working with the homeless community for 24 years. As a small business owner Sam housed homeless individuals in his florist, allowing them to spend the night in the florist after business hours.  As a community member he befriended and supported homeless individuals, welcoming them into his life.</br>Following the City's lead in theHousing First Initiative, Sam is coordinating a comprehensive pilot project.  The pilot, the Hugh Street Housing project, is located  in Pearl City and will house 20+ modular houses for homeless or at-risk families.</br>This site is currently undergoing environmental review.  If it is approved, City and County will fund construction costs. The operation and maintenance will be provided through a public-private partnership.</br>Sam hopes this project will be a model for a systematic cooperative to plan and build, streamlining present practices.  This would be the first of multiple projects to house  homeless and at-risk  individuals and families in Hawaii."
