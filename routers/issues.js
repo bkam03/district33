de2d0626a5d31854220bff40bcd349ff1f700501
BK <bkam03@yahoo.com>
2018-03-13 21:00:46 -1000

    changed lets to vars

--- routers/issues.js
+++ routers/issues.js
@@ -7,22 +7,22 @@ const router = express.Router();
 const issuesArray = issuesData.issues;
 
 router.get("/education", (req, res) => {
-  let educationData = issuesArray[0];
+  var educationData = issuesArray[0];
   res.render("issue", educationData);
 });
 
 router.get("/community", (req, res) => {
-  let communityData = issuesArray[1];
+  var communityData = issuesArray[1];
   res.render("issue", communityData);
 });
 
 router.get("/business", (req, res) => {
-  let businessData = issuesArray[2];
+  var businessData = issuesArray[2];
   res.render("issue", businessData);
 });
 
 router.get("/homeless", (req, res) => {
-  let homelessData = issuesArray[3];
+  var homelessData = issuesArray[3];
   res.render("issue", homelessData);
 });
 
