afd15b8e86dc875b7f6c156ec8842617cf6bf8a3
BK <bkam03@yahoo.com>
2018-03-26 10:51:15 -1000

    added homeless, education text changes

--- public/js/issuesData.js
+++ public/js/issuesData.js
@@ -6,33 +6,59 @@ const issuesArray = [
     subheading: "",
     link: "education",
     description:
-      "Sam believes hawaii’s children deserve a high quality education provided by engaged, empowered teachers.  Sam is currently Vice Chairman if the House Education Committee. He has also been formally and informally involved in the curriculum development, operations and facility maintenance of the schools in Aiea and Halawa.   As a community member Sam has been on the board of Gus Webling Elementary  School.  As a legislator  sam has obtained the following funding and/or support for projects.As a member of the House Education Committer Sam hopes to advocate for both students and teachers through his role on this pivotal committee. He is committed to providing both high quality educational opportunities to Hawaii’s children and supporting Hawaii’s teachers.",
+      "When educators developed the School community-based management (SCBM) 20 years ago, Representative Sam Kong  became Gus  Webling Elementary School's Community Representative; he has been there ever since.  Upon being elected as state Representative, he can  no longer officially be the community representative, but still attends meetings though he does not have voting rights.</br></br>Representative Kong stays connected with curriculum development, operations and facility maintenance and also assists the school as it transitions through different administrations.Representative Kong has two children that have gone through the Aiea school system from elementary through high school;  his younger daughter recently graduated from Aiea High School.  As a parent Representative Kong knows how important it is to be involved  with your keiki’s education. He recently met with Aiea High School’s new principal to understand how he can best support the high school.  Representative Kong is leading the Drag Racing Against Gangs and Graffiti (<a href='http://www.draggteam.org'>DRAGG</a>) program at Aiea High School (AHS), a collaborative project between AHS, Local Law Enforcement,  and world class race drivers to support the school’s auto shop program and increase participation by at risk youth. </br></br>Representative Kong believe hawaii’s keiki deserve a high quality education provided by engaged, empowered teaches. He is currently Vice Chair of the House  Education Committee and hopes to advocate for students, teachers and administration through his role on this pivotal committee. He is committed to providing both high quality educational opportunities to Hawaii’s keiki and supporting Hawaii’s teachers.</br></br>Funds allocated to Aiea area schools under Representative Kong’s tenure: ",
 
-    listTitle: "CIP Projects",
+    listTitle: "CIPS 2017-2018",
     listItems: [
       {
         itemTitle: "Pearl Ridge Elementary School",
-        itemDescription:
-          "Design and construction for air conditioning improvements for buildings H & J",
-        itemEnd: "Total Funding:  1,166,500.00"
-      },
-      {
-        itemTitle: "Aiea  Intermediate School",
-        itemDescription:
-          "Design and construction for improvements to the cafeteria stage.",
-        itemEnd: "Total Funding: $450,000"
+        projects : [
+          {
+            itemDescription:
+            "Design and construction for central courtyard improvements; ground and site improvements.",
+            itemEnd: "Total Funding:  $135,900"
+          }
+        ]
       },
       {
         itemTitle: "Webling Elementary School",
-        itemDescription:
-          "To finance construction for new playground equipment and site improvements.",
-        itemEnd: "Total Funding: $125,000"
+        projects : [
+          {
+            itemDescription:
+            "To finance construction for new playground equipment and site improvements.",
+            itemEnd: "Total Funding: $125,000"
+          }
+        ]
+      },
+      {
+        itemTitle: "Aiea  Intermediate School",
+        projects : [
+          {
+            itemDescription:
+            "Design and construction for improvements to the cafeteria stage.",
+            itemEnd: "Total Funding: $450,000"
+          },
+          {
+            itemDescription:
+            "Design, construction, and equipment for an applied technology center.",
+            itemEnd: "Total Funding: $3,200,000"
+          },
+          {
+            itemDescription:
+            "Construction and equipment to complete cafeteria renovation; ground and site improvements; equipment and appurtenances. ",
+            itemEnd: "Total Funding: $550,000"
+          },
+        ]
       },
       {
         itemTitle: "Aiea High School",
-        itemDescription:
-          "Design, construction, and equipment for girls’ athletic locker room, and other athletic facilities.",
-        itemEnd: "Total  Funding: $3,900,000"
+        projects : [
+          {
+            itemDescription:
+            "Design, construction, and equipment for a new building, and renovation and expansion for a girls’ athletic locker room, weight training facility, and other athletic facilities; ground and site improvements; equipment and appurtenances.",
+            itemEnd: "Total Funding: $3,900,000"
+          }
+        ]
       }
     ]
   },
@@ -41,7 +67,7 @@ const issuesArray = [
     subheading: "",
     link: "/community",
     description:
-      "Back to the Graffiti Busters- As a volunteer, we were so good. We had distributed so much paint that we had graffiti under control for a long time. So we disbanded.  Well, all good things come to an end, I guess.  Graffiti came back but someone was beating me to it and covering it up!  I found out who when I got elected but I fell ill so I couldn't participate. We did get together later on though and I joined their team- The Lions Club.   Lions Club members from different clubs get together to do different projects taking care of the community.</br> Wanting to focus on Aiea, we started doing many projects in my district. Well from Salt Lake to Pearl City to be honest.  Though we do go out to Kailua, Kalihi and Chinatown on occasion.  We have done so many projects in Aiea and Pearl City each Saturday morning, that I've lost count.  But I couldn't imagine what our community would look like if we didn't.   From graffiti painting, landscaping, to just picking up trash, Lions have done so much that you may have not even noticed.  Thank you to those also who have done things out there in the community.  Oh yes. I may not know who you are, but I do know the work you do. It has not gone unnoticed."
+      "Back to the Graffiti Busters- As a volunteer, we were so good. We had distributed so much paint that we had graffiti under control for a long time. So we disbanded.  Well, all good things come to an end, I guess.  Graffiti came back but someone was beating me to it and covering it up!  I found out who when I got elected but I fell ill so I couldn't participate. We did get together later on though and I joined their team- The Lions Club.   Lions Club members from different clubs get together to do different projects taking care of the community.</br></br> Wanting to focus on Aiea, we started doing many projects in my district. Well from Salt Lake to Pearl City to be honest.  Though we do go out to Kailua, Kalihi and Chinatown on occasion.  We have done so many projects in Aiea and Pearl City each Saturday morning, that I've lost count.  But I couldn't imagine what our community would look like if we didn't.   From graffiti painting, landscaping, to just picking up trash, Lions have done so much that you may have not even noticed.  Thank you to those also who have done things out there in the community.  Oh yes. I may not know who you are, but I do know the work you do. It has not gone unnoticed."
   },
   {
     issue: "Business",
@@ -55,7 +81,7 @@ const issuesArray = [
     subheading: "",
     link: "/homeless",
     description:
-      "Despite the state's efforts in providing housing, homelessness continues to rise in Hawaii.  According to the Department of Urban Development, Hawaii has the highest rate of homelessness per capita in the United States. Homelessness is a complex issue with multiple causes. Sam strongly believes that housing is a  key priority.   He has personally been working with the homeless community for 24 years. As a small business owner Sam housed homeless individuals in his florist, allowing them to spend the night in the florist after business hours.  As a community member he befriended and supported homeless individuals, welcoming them into his life.</br>Following the City's lead in theHousing First Initiative, Sam is coordinating a comprehensive pilot project.  The pilot, the Hugh Street Housing project, is located  in Pearl City and will house 20+ modular houses for homeless or at-risk families.</br>This site is currently undergoing environmental review.  If it is approved, City and County will fund construction costs. The operation and maintenance will be provided through a public-private partnership.</br>Sam hopes this project will be a model for a systematic cooperative to plan and build, streamlining present practices.  This would be the first of multiple projects to house  homeless and at-risk  individuals and families in Hawaii."
+      'When Representative Kong was the owner of Aiea Florist he let homeless individuals  spend the night in his florist. Solving the homeless crisis is a very personal, sincere issue for him, not a political platform.</br></br>Despite the state’s efforts in providing housing, homelessness continues to rise in Hawaii. According to the Department of Urban Development Hawaii has the highest rate of homeless per capita in the United States. Homelessness is a complex issue with multiple causes.</br></br>Representative Sam Kong’s goal is to address the homeless situation on Oahu through providing additional housing for homeless individuals and their families.  After personally assisting homeless individuals in the Aiea community for the past twenty five years, Representative Kong has  made homelessness a priority as an elected official.  His plan began with developing a team of government, business and and community leaders who could implement housing  solutions. They have identified properties throughout Oahu (City, State, and Federal) for possible development. Their first project is "Under and Upwards," a six acre Department  of transportation  (DOT) property located under the freeway in Pearl City off of Hugh St.  Representative Kong and his team  have recently received approval by the Pearl City neighborhood Board to continue with this project.</br></br>The second project is the Cedar Church  four acre property located on Waianae Valley Road.  (Crystal, can u add  more herr?) The third project is Hoomau Ke Ola, a 1,000-acre parcel which would serve  the chronically ill and drug abuse clients; This site is also located in Waianae.  The fourth project is a 3 ½ acre federal property Which will focus on meeting  the needs of homeless  veterans.  The land for these projects have been donated by various entities.  The planning  and design  of the first project has been done by a local architectural firm at minimal cost to the state. The building and operations of the housing will be a public-private endeavor.  The specific operational details such as admission criteria will be determined by the local community. Representative Kong is confident that implementing these projects will house homeless individuals and families in a cost conscious manner, and will be of  significant benefit to our community.'
   }
 ];
 
